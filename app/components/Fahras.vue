785c11bc950a52f94977603066815e67f8a59c50
Peace Marshal <88880103+peace2074@users.noreply.github.com>
2025-04-09 19:29:36 +0300

    bookmarks 2/3

--- app/components/Fahras.vue
+++ app/components/Fahras.vue
@@ -1,31 +1,31 @@
 <script lang="ts" setup>
 import { useRouter } from '#imports'
 
-const { $holybook } = useNuxtApp()
-interface ONET {
-  Index: number | string
-  name: string
-  Location: string
-  TotalVerses: number
-  Verses: string[]
-}
-const quran: Ref<ONET[]> = ref($holybook)
+// const { $holybook } = useNuxtApp()
+// interface ONET {
+//   Index: number | string
+//   name: string
+//   Location: string
+//   TotalVerses: number
+//   Verses: string[]
+// }
+// const quran: Ref<ONET[]> = ref($holybook)
 
-watch(
-  $holybook,
-  (val) => {
-    quran.value = val[0]
-  },
-  { deep: true },
-)
-onNuxtReady(() => {
-  if ($holybook) {
-    quran.value = $holybook
-  }
-})
+// watch(
+//   $holybook,
+//   (val) => {
+//     quran.value = val[0]
+//   },
+//   { deep: true },
+// )
+// onNuxtReady(() => {
+//   if ($holybook) {
+//     quran.value = $holybook
+//   }
+// })
 
-const names = $holybook.map(v => ({ names: v.name }))
-const router = useRouter()
+// const names = $holybook.map(v => ({ names: v.name }))
+// const router = useRouter()
 
 function navToLok(lok: number) {
   const advonced = lok + 1 || 1
@@ -34,12 +34,13 @@ function navToLok(lok: number) {
 </script>
 
 <template>
-  <ol v-if="$holybook" class="column q-mt-xl q-pt-lg text-ceter">
+  <!-- <ol v-if="$holybook" class="column q-mt-xl q-pt-lg text-ceter">
     <nuxt-link
       v-for="(i, ind) in names" :key="i.names" class="text-ceter q-mx-xs cursor-pointer"
       @click="navToLok(ind)"
     >
       {{ ind + 1 }}-{{ i.names }}
-    </nuxt-link>
-  </ol>
+    </nuxt-link> -->
+  <!-- </ol> -->
+  <q-skeleton type="QInput" class="fit" />
 </template>
