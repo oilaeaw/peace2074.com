01641760ca4894279a3bd9c319038f0a3ace0304
Peace Marshal <88880103+peace2074@users.noreply.github.com>
2026-02-06 19:11:38 +0200

    dsfdafa

---
+++ dist/assets/QCardActions-B2c2I0YJ.js
@@ -0,0 +1,1 @@
+import{c as e,aK as c,aL as r,b as n,h as l,ad as i}from"./index-mTTMThk-.js";const d=e({name:"QCardActions",props:{...c,vertical:Boolean},setup(a,{slots:s}){const o=r(a),t=n(()=>`q-card__actions ${o.value} q-card__actions--${a.vertical===!0?"vert column":"horiz row"}`);return()=>l("div",{class:t.value},i(s.default))}});export{d as Q};
