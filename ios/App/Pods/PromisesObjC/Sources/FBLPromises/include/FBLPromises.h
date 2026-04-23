262a94f9b74476c8e9c3c41de8b13c6e80eb1d3b
Peace Marshal <88880103+peace2074@users.noreply.github.com>
2026-04-23 14:21:10 +0300

    Refactor code structure for improved readability and maintainability

---
+++ ios/App/Pods/PromisesObjC/Sources/FBLPromises/include/FBLPromises.h
@@ -0,0 +1,32 @@
+/**
+ Copyright 2018 Google Inc. All rights reserved.
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at:
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+
+#import <FBLPromises/FBLPromise+All.h>
+#import <FBLPromises/FBLPromise+Always.h>
+#import <FBLPromises/FBLPromise+Any.h>
+#import <FBLPromises/FBLPromise+Async.h>
+#import <FBLPromises/FBLPromise+Await.h>
+#import <FBLPromises/FBLPromise+Catch.h>
+#import <FBLPromises/FBLPromise+Delay.h>
+#import <FBLPromises/FBLPromise+Do.h>
+#import <FBLPromises/FBLPromise+Race.h>
+#import <FBLPromises/FBLPromise+Recover.h>
+#import <FBLPromises/FBLPromise+Reduce.h>
+#import <FBLPromises/FBLPromise+Retry.h>
+#import <FBLPromises/FBLPromise+Then.h>
+#import <FBLPromises/FBLPromise+Timeout.h>
+#import <FBLPromises/FBLPromise+Validate.h>
+#import <FBLPromises/FBLPromise+Wrap.h>
