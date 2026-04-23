32d0d1d499b838d1a7197db5cb5264a748b9a320
Peace Marshal <88880103+peace2074@users.noreply.github.com>
2026-04-23 23:18:06 +0300

    fix: update tsconfig.json to remove typeRoots and add ignoreDeprecations option

--- peace2074-mobile/app/components/Home.vue
+++ peace2074-mobile/app/components/Home.vue
@@ -1,19 +1,32 @@
 <template>
-    <Frame>
-        <Page>
-            <ActionBar>
-                <Label text="Home"/>
-            </ActionBar>
+  <Frame>
+    <Page>
+      <ActionBar>
+        <Label text="Peace2074" />
+      </ActionBar>
 
-            <GridLayout>
-                <Label class="text-xl align-middle text-center text-gray-500" :text="message" />
-            </GridLayout>
-        </Page>
-    </Frame>
+      <GridLayout rows="auto,auto" verticalAlignment="center">
+        <Label
+          row="0"
+          text="Peace2074"
+          textAlignment="center"
+          fontSize="28"
+          color="#0A6B44"
+        />
+        <Label
+          row="1"
+          :text="message"
+          textAlignment="center"
+          fontSize="16"
+          color="#4B5563"
+        />
+      </GridLayout>
+    </Page>
+  </Frame>
 </template>
 
 <script setup lang="ts">
-  import { computed } from 'nativescript-vue'
+import { computed } from 'nativescript-vue'
 
-  const message = computed(() => "Blank {N}-Vue app");
+const message = computed(() => 'The Noble Quran')
 </script>
