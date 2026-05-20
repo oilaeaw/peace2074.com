29f4a76de496c9dbfba828eb63dd5df228be18bf
Wael Wahbeh <wahbehw@gmail.com>
2026-05-20 10:42:21 +0300

    chore: add projectName to nativescript configuration

--- ios/nativescript.config.ts
+++ ios/nativescript.config.ts
@@ -2,6 +2,7 @@ import { NativeScriptConfig } from '@nativescript/core'
 
 export default {
   id: 'com.peace2074.app',
+  projectName: 'peace2074mobile',
   appPath: 'app',
   appResourcesPath: 'App_Resources',
   android: {
